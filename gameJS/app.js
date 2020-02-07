0675fbbce6e489d73863b06ee605ecf29bfa2a77
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-02-06 22:37:21 -0500

    cleaning code

--- gameJS/app.js
+++ gameJS/app.js
@@ -14,20 +14,10 @@ wordOne.status;
 const message = document.querySelector("#message");
 message.textContent = wordOne.getStatusMessage();
 
-// const initialMessage = document.querySelector("#initialMessage");
-// initialMessage.textContent = wordOne.Status;
-
-console.log(wordOne.status);
-
-// console.log(wordOne.getStatus());
-
 window.addEventListener("keypress", e => {
   const keypress = String.fromCharCode(e.charCode);
   wordOne.getGuesses(keypress);
   getPuzzel.textContent = wordOne.getPuzzle();
   guesses.textContent = wordOne.guesses;
-
-  // console.log(wordOne.status);
-  // console.log(wordOne.getStatusMessage());
   message.textContent = wordOne.getStatusMessage();
 });
