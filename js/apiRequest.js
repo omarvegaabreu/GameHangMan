666e931fe290c6b069b009eb7e9f202c8166e296
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-02-14 22:24:42 -0500

    api promise done

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -1,9 +1,8 @@
 "use strict";
 
-const getPuzzle = (wordCount, callback) => {
-  const request = new XMLHttpRequest();
-
-  request.addEventListener("readystatechange", e => {
+const getPuzzle = new Promise(wordCount) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", e => {
     if (e.target.readyState === 4 && e.target.status === 200) {
       const data = JSON.parse(e.target.responseText);
       callback(undefined, data.puzzle);
@@ -14,7 +13,10 @@ const getPuzzle = (wordCount, callback) => {
 
   request.open("GET", `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
   request.send();
-};
+}
+
+
+
 
 const getCountry = (countryCode, callback) => {
   const countryRequest = new XMLHttpRequest();
